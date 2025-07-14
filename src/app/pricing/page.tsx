2444c339380a379b92349b097e238375040134a6
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2025-07-14 09:50:39 +0530

    feat: Add pricing page with Stripe pricing table integration

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,9 @@
+function page() {
+  return (
+    <div><script async src="https://js.stripe.com/v3/pricing-table.js"></script>
+<stripe-pricing-table pricing-table-id="prctbl_1RkHaZ2UEsSbvoM0rFUltybU"
+publishable-key="pk_test_51Rjj0Q2UEsSbvoM0yh3tqhHfwDmz7hRhwmHUsGF6QL7aKBc1eTTIodavrclPfqjPSCewv5TU89UfDR7IG9nOIfUq00d6fqfr2k">
+</stripe-pricing-table></div>
+  )
+}
+export default page
